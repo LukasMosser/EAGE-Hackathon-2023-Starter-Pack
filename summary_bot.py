e02886e016319c70063783bd200f58ecbabfc847
Lukas Mosser <lukas.mosser@gmail.com>
2023-05-19 19:12:58 +0200

    added slack bot comments

--- summary_bot.py
+++ summary_bot.py
@@ -5,9 +5,8 @@ from modal import Image, Secret, Mount, Stub, web_endpoint
 
 stub = Stub("eage-annual-2023-hackathon-summary-bot")
 
-CACHE_PATH = "/root/eage_annual_2023_summaries_basic_test"
 
-
+# Create a stub function that will do the reverse search and generate the response
 @stub.function(
     image=(
         Image.debian_slim().pip_install(
@@ -35,10 +34,14 @@ async def run_slack_bot(question: str, channel_name: Optional[str] = None):
 
     # Create a base embeddings api object
     embeddings = OpenAIEmbeddings()
+
+    # Load the vector db
     db = FAISS.load_local("/root/eage_annual_2023_summaries_basic_test", embeddings)
 
+    # query the vector db
     query_result = db.similarity_search(question, k=7)
 
+    # create the context from the returned strings
     context = " ".join(
         [
             "Summary {0:}: {1:}".format(i, q.page_content)
@@ -46,6 +49,7 @@ async def run_slack_bot(question: str, channel_name: Optional[str] = None):
         ]
     )
 
+    # create a completion with the openai API
     completion = openai.Completion.create(
         model="text-davinci-003",
         prompt=f"The following content is contextual information: {context} \n"
@@ -57,12 +61,14 @@ async def run_slack_bot(question: str, channel_name: Optional[str] = None):
 
     result = completion.choices[0].text
 
+    # Write back the result to slack via the other stub function
     if channel_name:
         write_query_to_slack.call(channel_name, result)
 
     return query_result
 
 
+# Entry point for the chat bot
 @stub.function()
 @web_endpoint(method="POST")
 async def entrypoint(request: Request):
@@ -72,6 +78,7 @@ async def entrypoint(request: Request):
     return f"Running query for: {prompt}."
 
 
+# Stub function to write the resulting message into slack
 @stub.function(
     image=Image.debian_slim().pip_install("slack-sdk"),
     secret=Secret.from_name("eage-summary-bot-token"),
@@ -79,10 +86,12 @@ async def entrypoint(request: Request):
 def write_query_to_slack(channel_name: str, query_result: str):
     import slack_sdk
 
+    # Create a slack client and write a message
     client = slack_sdk.WebClient(token=os.environ["SLACK_BOT_TOKEN"])
     client.chat_postMessage(channel=channel_name, text=query_result)
 
 
+# Local stub for debugging
 @stub.local_entrypoint()
 def run(prompt: str = "What abstracts are related to CO2 sequestration"):
     query_result = run_slack_bot.call(prompt)
